1dc7be2d36622e93f5b082e040c9d55f55ed2334
Jacob <jacob.atam@gmail.com>
2024-01-19 17:48:46 -0800

    fix

--- database.php
+++ database.php
@@ -1,78 +1,73 @@
-<?php 
+<?php
 
 $about = [
-	'teaser'=>'Vinnex Investment Services Limited (VISL) is an indigenous marine, energy & petroleum  (E&P) services company incorporated and registered in June 2007 to carry out such businesses but not limited to the under  listed.  <a href="/about_us.php">Read More</a> ',
-	'des'=>''
+	'teaser' => 'VISL is focused on products and services that have been structured to meet every of your company’s/shipping needs such as the following: <a href="/about_us.php">Read More</a> ',
+	'des' => ''
 ];
 
 $services = [
-	
-		'Marine Services'=>[
-			'image'=>'bunkering',
-			'slug'=>'marine_services',
-			
-			'teaser'=>'As a multi-level Nigerian company.',
-			 'description'=>'As a multi-level Nigerian company, VISL was registered to provide specialized handling of corporate transactions involving marine services, spares, energy, consultancy and logistics. We are poised to providing services on a world class level both locally and internationally with our dynamic and professional team in the field having had special know-how in: marine services, spares, energy related logistics and supply chains management, procurement, sales, distribution, haulage and consultancy to world class companies
+
+	'Marine Services' => [
+		'image' => 'bunkering',
+		'slug' => 'marine_services',
+
+		'teaser' => 'As a multi-level Nigerian company.',
+		'description' => 'As a multi-level Nigerian company, VISL was registered to provide specialized handling of corporate transactions involving marine services, spares, energy, consultancy and logistics. We are poised to providing services on a world class level both locally and internationally with our dynamic and professional team in the field having had special know-how in: marine services, spares, energy related logistics and supply chains management, procurement, sales, distribution, haulage and consultancy to world class companies
 
 			 Our capacity for marine services deployment enables us provide special ship-handling, supply of fresh water, supply of AGO, LPFO Bunker and other marine services. As a logistics we are charged to supply and transport goods and services to where they are needed including office sundry materials, stationeries, computer accessories and security equipments, etc.
 			 
 			 We are well equipped and fully backed many years of professional practice to handle more than 70 ships annually. See pages 10 - 11 for a brief on the company activities. We have attained this feat thus far not accidentally but because we are determined consciously build and yet build on the foundation of honesty and dedication bearing in mind the profound demands of ethics and professionalism in the marine/E&P industry which are strictly in conformity with the API standards. It is therefore a great pleasure to be identified as one of the registered marine company with the Department of Petroleum Resources (DPR).  '
-		],
-		'Diesel Bunker Supply'=>[
-				'image'=>'fuel_oil',
-				'teaser'=>'We supply bunker fuels, lubricants AGO, PMS, IFO and related products and services.
+	],
+	'Diesel Bunker Supply' => [
+		'image' => 'fuel_oil',
+		'teaser' => 'We supply bunker fuels, lubricants AGO, PMS, IFO and related products and services.
 				',
-				'description'=>'We supply bunker fuels, lubricants AGO, PMS, IFO and related products and services.
+		'description' => ' VISL guarantees a reliable and compliant supply of marine diesel fuels, meeting
+				the stringent standards of the industry. Our commitment to quality ensures optimal performance and environmental responsibility..
 				 '
-		],
-		'Supply Chain Management (SCM)'=>[
-				'image'=>'gas_oil',
-				'teaser'=>'Is the oversight of each step in the supply chain process, here we manage problems, purchases,',
-				'description'=>'is the oversight of each step in the supply chain process, here we manage problems, purchases, inventory and transportation, SCM helps our customers to keep track of their products. '
-						],
-		'Marine Security Services'=>[
-				'image'=>'water',
-				'teaser'=>'We provide adequate maritime security , escorts  with the help of Nigerian Navy .',
-				'description'=>'Security is a serious issue considering the rate of insecurity threat emanating from
-				everywhere at sea, as a result pirate activities that are on the increase in African
-				waters.
-				
-				It is the primary and constitutional responsibility of the Nigeria Navy to provide
-				security and police sea way for the safety of every vessel within Nigeria Territorial
-				Waters (EEZ), and this is the service that the Nigeria Navy has been 
-				Rendering but the security has never been 100% guaranteed. The maritime security
-				services arm of Vinnex  Investment Services Ltd, is registered with the Navy for this
-				purpose
-				',
-				
-		],
-	    'Ship Store Suppliers'=>[
-			'image'=>'water',
-			'teaser'=>'We offer the most comprehensive marine supply options available by combining..',
-			'description'=>'We offer the most comprehensive marine supply options available by combining reliable services and competitive pricing with a global support network. Our experience and growing number of international distribution centres allows us to supply vessels of any type with a 100% fill-rate, and if you choose us to provision your vessel supply, we guarantee your experience will always be highlighted by world-class customer service and personalized attention.
-			',
-			
-		],
-		'Ship-to-ship operation (STS)'=>[
-			'image'=>'water',
-			'teaser'=>'We are always on standby to supply fresh water and AGO to vessels, with accuracy. ',
-			'description'=>'Good and safe drinking water is essential on-board your offshore platform or rig.  Be sure to get clean and portable water from us at Bervas Marine.',
-			
-		],
-		'Deck Store Suppliers'=>[
-			'image'=>'water',
-			'teaser'=>'We supply ambient, chilled, frozen and bonded foods as well as soft & alcoholic duty-free beverages e.t.c.',
-			'description'=>'Good and safe drinking water is essential on-board your offshore platform or rig.  Be sure to get clean and portable water from us at Bervas Marine.',
-			
-		],
-		' Transportation and Distribution (Haulage)'=>[
-			'image'=>'water',
-			'teaser'=>'Haulage of petroleum products across Nigeria and abroad , we also transport Fresh water to vessels and companies.',
-			'description'=>'Haulage of petroleum products across Nigeria and abroad , we also transport Fresh water to vessels and companies .',
-			
-		],
+	],
+	'Supply Chain Management (SCM)' => [
+		'image' => 'gas_oil',
+		'teaser' => 'Is the oversight of each step in the supply chain process, here we manage problems, purchases,',
+		'description' => 'Leveraging cutting-edge technology, VISL optimizes supply chain processes for efficiency
+		and cost-effectiveness. Our SCM services cover the entire logistics spectrum, ensuring seamless operations for our clients. '
+	],
+	'Marine Security Services' => [
+		'image' => 'water',
+		'teaser' => 'We provide adequate maritime security , escorts  with the help of Nigerian Navy .',
+		'description' => "Security is paramount in maritime operations. VISL's dedicated team offers
+		comprehensive marine security services, safeguarding vessels and personnel against potential threats, piracy, and unauthorized access.",
+
+	],
+	'Ship Store Suppliers' => [
+		'image' => 'water',
+		'teaser' => 'We offer the most comprehensive marine supply options available by combining..',
+		'description' => 'Our ship store supply service offers a comprehensive range of provisions, from safety gear to
+		daily consumables. VISL is the trusted partner for all vessel-related operational requirements.'
+		,
+
+	],
+	'Ship-to-ship operation (STS)' => [
+		'image' => 'water',
+		'teaser' => 'We are always on standby to supply fresh water and AGO to vessels, with accuracy. ',
+		'description' => 'Facilitating safe and efficient ship-to-ship transfer operations, VISL ensures compliance
+		with international regulations, enabling vessels to transfer fuel or cargo at sea with confidence.',
+
+	],
+	'Deck Store Suppliers' => [
+		'image' => 'water',
+		'teaser' => 'We supply ambient, chilled, frozen and bonded foods as well as soft & alcoholic duty-free beverages e.t.c.',
+		'description' => 'Good and safe drinking water is essential on-board your offshore platform or rig.  Be sure to get clean and portable water from us at Bervas Marine.',
+
+	],
+	'  Deck Store Supply:' => [
+		'image' => 'water',
+		'teaser' => 'VISL supports seamless vessel functioning by providing a diverse array of deck store
+		supplies,',
+		'description' => 'VISL supports seamless vessel functioning by providing a diverse array of deck store
+		supplies, including navigational equipment, safety gear, and maintenance tools.',
+
+	],
 
 
 ]
-
-?>
