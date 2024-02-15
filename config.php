ef79bf478163d2a37ec0b0f6023a39ccbde54e21
Jacob <jacob.atam@gmail.com>
2024-02-15 14:04:58 -0800

    fix

--- config.php
+++ config.php
@@ -1,13 +1,10 @@
-<?php 
+<?php
 
- $settings =[
- 	'site_name'=>'Vinnex Services Limited',
- 	'site_title'=>'',
- 	'site_meta_title'=>'Marine Services, Energy, Spares and Logistics',
- 	'site_phone'=>' +234 8178765125,+234 906 892 5416',
-	 'site_email'=>'info@vinnexservices.com,Iyaduah@vinnexservices.com',
-	 'site_address'=>'8 payne cresent apapa, Lagos'
-
-
- ]
-?>
+$settings = [
+	'site_name' => 'Vinnex Services Limited',
+	'site_title' => '',
+	'site_meta_title' => 'Marine Services, Energy, Spares and Logistics',
+	'site_phone' => ' +234 8178765125,+234 906 892 5416',
+	'site_email' => 'info@vinnexservices.com / bunkers@vinnexservices.com',
+	'site_address' => '8 Payne Crescent Apapa, Lagos,Nigeria.'
+]
